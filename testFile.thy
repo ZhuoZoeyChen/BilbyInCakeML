18cd6f3babf314644ba67b7878d7d147ca5cba5f
Zoey Chen <zoey.chen@csiro.au>
2020-01-24 15:04:42 +1100

    up to line 177 (^sub), line 220 (mapsto). attempted code in notes.txt

--- testFile.thy
+++ testFile.thy
@@ -6,3 +6,8 @@ begin
 
 
 definition "a = select {x. x \<ge> time'}"
+
+(afs, r) <- afs_update afs (\f. f(dir_ino |-> (i_dir_update (\d. d(name:=option.None)) (the $ f dir_ino)\<lparr>i_ctime:=time,i_mtime:=time\<rparr>),
+                                            v_ino vnode \<mapsto> inode));
+
+end
